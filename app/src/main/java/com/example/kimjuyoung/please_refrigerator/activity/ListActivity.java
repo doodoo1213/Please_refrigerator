6ad74e765a27580b1c8a0cf0020cd0b97c699240
dlwoduu <dlwodusqmdl@naver.com>
2017-12-04 03:30:41 +0900

    -list

--- app/src/main/java/com/example/kimjuyoung/please_refrigerator/activity/ListActivity.java
+++ app/src/main/java/com/example/kimjuyoung/please_refrigerator/activity/ListActivity.java
@@ -1,11 +1,21 @@
 package com.example.kimjuyoung.please_refrigerator.activity;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.kimjuyoung.please_refrigerator.R;
+
 
 /**
  * Created by Kim juyoung on 2017-12-04.
  */
 
 public class ListActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+      //  setContentView(R.layout.activity_list);
+
+    }
 
 }
